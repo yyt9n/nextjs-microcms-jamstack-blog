a071e159427e957c51b9ef07c1dd62a67bcabf36
yyt9n <h.yyt9n@gmail.com>
2022-12-17 17:11:44 +0900

    first commit

---
+++ pages/blog/[id].js
@@ -0,0 +1,38 @@
+import { client } from "../../libs/client";
+import styles from '../../styles/Home.module.scss';
+
+export default function BlogId({blog}){
+    return (
+        <main className={styles.main}>
+            <h1 className={styles.title}>{blog.title}</h1>
+            <p className={styles.publishedAt}>{blog.publishedAt}</p>
+            <div
+                dangerouslySetInnerHTML={{
+                    __html: `${blog.body}`,
+                }}
+                className={styles.post}
+            />
+      </main>
+    )
+}
+
+export const getStaticPaths = async () => {
+    const data = await client.get({ endpoint: "blog" });
+    const paths = data.contents.map((content)=>`/blog/${content.id}`);
+
+    return {
+        paths,
+        fallback: false
+    };
+}
+
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const data = await client.get({ endpoint: "blog", contentId: id });
+
+    return {
+        props: {
+            blog: data,
+        },
+    };
+}
